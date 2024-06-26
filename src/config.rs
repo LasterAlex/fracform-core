1e0d8e9313d9c76931cf4b78551c98b782a0fd85
LasterAlex <sasha.ishukshin@gmail.com>
2024-06-26 03:52:03 +0300

    Added basic fractal generation

---
+++ src/config.rs
@@ -0,0 +1,4 @@
+pub const DIMENSIONS: (i32, i32) = (2000, 2000);
+pub const BUDDHA_ITER_RGB: (u32, u32, u32) = (10000, 1000, 100);
+pub const ITERS: u32 = 1000;
+pub const STACK_SIZE: usize = 512 * 1024 * 1024 as usize;
